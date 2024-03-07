d3d809c4b71967ba9c59a5a29f7fae003ad69a47
Olegxck <60327887+olegnck404@users.noreply.github.com>
2024-03-07 08:29:15 +0200

    Task 4

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -4,99 +4,189 @@ import java.util.*;
 public class CalculatorApp {
 
     public static void main(String[] args) {
-        // Створюємо колекцію для зберігання результатів обчислень
-        List<ParametersAndResults> dataList = new ArrayList<>();
+        // 1. Зберігання результатів обчислень у колекції
 
-        // Додаємо результати обчислень до колекції
+        // 1.1 Додати колекцію до класу ParametersAndResults
+        class ParametersAndResults implements Serializable {
+
+            private double[] parameters;
+            private List<CalculationResult> results;
+
+            public ParametersAndResults(double[] parameters, double[] results) {
+                this.parameters = parameters;
+                this.results = new ArrayList<>();
+                for (double result : results) {
+                    this.results.add(new CalculationResult("Default", result));
+                }
+            }
+
+            public void addResult(CalculationResult result) {
+                results.add(result);
+            }
+
+            public List<CalculationResult> getResults() {
+                return results;
+            }
+        }
+
+        // 1.2 Створити клас CalculationResult для зберігання результатів
+        class CalculationResult implements Serializable {
+
+            private String type;
+            private double value;
+
+            public CalculationResult(String type, double value) {
+                this.type = type;
+                this.value = value;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public double getValue() {
+                return value;
+            }
+        }
+
+        // 1.3 Зберігати об'єкти ParametersAndResults в ArrayList
+        ArrayList<ParametersAndResults> dataList = new ArrayList<>();
+
         ParametersAndResults data1 = new ParametersAndResults(new double[]{1.0, 2.0, 3.0}, new double[]{4.0, 5.0, 6.0});
         dataList.add(data1);
 
         ParametersAndResults data2 = new ParametersAndResults(new double[]{7.0, 8.0, 9.0}, new double[]{10.0, 11.0, 12.0});
         dataList.add(data2);
 
-        // Зберігаємо та відновлюємо стан об'єкта з файлу
-        saveAndRestoreObjectState(dataList);
+        // 2. Розширення рахунку за допомогою Factory Method (Virtual Constructor)
 
-        // Проводимо тестування коректності результатів та серіалізації/десеріалізації
-        testCalculationAndSerialization();
+        // 2.1 Створити базовий клас AbstractCalculator
+        abstract class AbstractCalculator {
 
-        // Підрахунок кількості входжень кожної цифри у десятковому поданні цілого числа
-        int number = 12345;
-        int[] digitCounts = countDigits(number);
+            public abstract double calculate(double a, double b);
 
-        System.out.println("Counts of digits in " + number + ":");
-        for (int i = 0; i < digitCounts.length; i++) {
-            System.out.println("Digit " + i + ": " + digitCounts[i]);
-        }
-    }
-
-    // Клас для зберігання параметрів та результатів обчислень
-    public static class ParametersAndResults implements Serializable {
-        private double[] parameters;
-        private double[] results;
-
-        public ParametersAndResults(double[] parameters, double[] results) {
-            this.parameters = parameters;
-            this.results = results;
-        }
-
-        public double[] getParameters() {
-            return parameters;
-        }
-
-        public double[] getResults() {
-            return results;
-        }
-    }
-
-    // Функція для серіалізації та відновлення стану об'єкта з файлу
-    private static void saveAndRestoreObjectState(List<ParametersAndResults> dataList) {
-        try {
-            // Зберігання об'єкта в файл
-            FileOutputStream fileOut = new FileOutputStream("data.ser");
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-            objectOut.writeObject(dataList);
-            objectOut.close();
-            fileOut.close();
-            System.out.println("Object state saved successfully.");
-
-            // Відновлення об'єкта з файлу
-            FileInputStream fileIn = new FileInputStream("data.ser");
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-            List<ParametersAndResults> restoredData = (List<ParametersAndResults>) objectIn.readObject();
-            objectIn.close();
-            fileIn.close();
-            System.out.println("Object state restored successfully.");
-
-            // Вивід відновлених даних
-            for (ParametersAndResults data : restoredData) {
-                System.out.println("Parameters: " + Arrays.toString(data.getParameters()));
-                System.out.println("Results: " + Arrays.toString(data.getResults()));
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Функція для тестування коректності результатів та серіалізації/десеріалізації
-    private static void testCalculationAndSerialization() {
-        // Реалізація тестувальної логіки
-    }
-
-    // Функція для підрахунку кількості входжень кожної цифри у десятковому поданні цілого числа
-    public static int[] countDigits(int number) {
-        int[] digitCounts = new int[10]; // Масив для зберігання кількості цифр 0-9
-
-        if (number == 0) {
-            digitCounts[0] = 1;  // Обробка спеціального випадку числа 0
-        } else {
-            while (number != 0) {
-                int digit = number % 10;
-                digitCounts[digit]++;
-                number /= 10;
+            public String getType() {
+                return "Abstract Calculator";
             }
         }
 
-        return digitCounts;
-    }
-}
+        // 2.2 Створити похідні класи для різних типів розрахунків
+        class AdditionCalculator extends AbstractCalculator {
+
+            @Override
+            public double calculate(double a, double b) {
+                return a + b;
+            }
+
+            @Override
+            public String getType() {
+                return "Addition Calculator";
+            }
+        }
+
+        class MultiplicationCalculator extends AbstractCalculator {
+
+            @Override
+            public double calculate(double a, double b) {
+                return a * b;
+            }
+
+            @Override
+            public String getType() {
+                return "Multiplication Calculator";
+            }
+        }
+
+        // 2.3 Використовувати Factory Method для створення екземплярів калькуляторів
+        public static AbstractCalculator createCalculator(String type) {
+            switch (type) {
+                case "Addition":
+                    return new AdditionCalculator();
+                case "Multiplication":
+                    return new MultiplicationCalculator();
+                default:
+                    throw new IllegalArgumentException("Unknown calculator type: " + type);
+            }
+        }
+
+        // 2.4 Приклад використання Factory Method
+        AbstractCalculator calculator = createCalculator("Addition");
+        double result = calculator.calculate(10, 20);
+        System.out.println("Result: " + result);
+
+        calculator = createCalculator("Multiplication");
+        result = calculator.calculate(10, 20);
+        System.out.println("Result: " + result);
+
+        // 3. Розширення ієрархії інтерфейсом "фабрикованих" об'єктів
+
+        // 3.1 Додати інтерфейс ResultFormatter
+        interface ResultFormatter {
+
+            String formatResult(CalculationResult result);
+        }
+
+        // 3.2 Реалізувати інтерфейс ResultFormatter для різних типів результатів
+        class TextResultFormatter implements ResultFormatter {
+
+            @Override
+            public String formatResult(CalculationResult result) {
+                return "Type: " + result.getType() + ", Value: " + result.getValue();
+            }
+        }
+
+        // 4. Реалізувати методи виведення результатів у текстовому виде
+        class HtmlResultFormatter implements ResultFormatter {
+
+            @Override
+            public String formatResult(CalculationResult result) {
+                return "<html>" +
+                        "<head><title>" + result.getType() + " Result</title></head>" +
+                        "<body>" +
+                        "<h1>" + result.getType() + "</h1>" +
+                        "<p>Value: " + result.getValue() + "</p>" +
+                        "</body>" +
+                        "</html>";
+            }
+        }
+
+        // 4. Реалізувати метод виведення результатів у табличному вигляді
+        class TextTableFormatter implements ResultFormatter {
+            private int columnWidth = 15; // Default column width
+
+            // Constructor for customization
+            public TextTableFormatter(int columnWidth) {
+                this.columnWidth = columnWidth;
+            }
+
+            @Override
+            public String formatResult(CalculationResult result) {
+                StringBuilder sb = new StringBuilder();
+                String headerFormat = "| %-" + columnWidth + "s | %-" + columnWidth + "s |\n";
+                String lineSeparator = String.format("+%s+%-s+\n", "-".repeat(columnWidth), "-".repeat(columnWidth));
+
+                sb.append(lineSeparator);
+                sb.append(String.format(headerFormat, "Type", "Value"));
+                sb.append(lineSeparator);
+                sb.append(String.format(headerFormat, result.getType(), result.getValue()));
+                sb.append(lineSeparator);
+                return sb.toString();
+            }
+        }
+
+        // 5. Розробити інтерфейс для "фабрикуючого" методу
+        interface CalculatorFactory {
+
+            AbstractCalculator createCalculator(String type);
+
+            ResultFormatter createFormatter(String type); // New factory method
+        }
+
+        // 6. Розробити клас для тестування основної функціональності
+        public static class CalculatorTester {
+            public static void test() {
+                // Add test cases here
+            }
+        }
+
+        // 7. Використати коментарі для автоматичної генерації документації засобами javad
